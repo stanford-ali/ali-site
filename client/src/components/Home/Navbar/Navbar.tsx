1b4fe84f8bb2e4a09a7ea55faffac124e0119690
Alix <acui@stanford.edu>
2020-07-19 15:21:12 -0500

    Add Navlinks + loader during auth

--- client/src/components/Home/Navbar/Navbar.tsx
+++ client/src/components/Home/Navbar/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = (props) => {
       <nav id="navbar">
         <div className="navbar">
           <Link to="/" className="navbar-item">
-            <img id="logo" src={require("../../assets/logo.svg")} alt="" />
+            <img id="logo" src={require("../../../assets/logo.svg")} alt="" />
           </Link>
           <Link to="/" className="navbar-item navbar-title">
             <span>Applied Learning Initiative</span>
