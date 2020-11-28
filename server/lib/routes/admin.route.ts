773727c724d6d5f0dddfc22801b6501386570ebf
David Toomer <djtoomer22@gmail.com>
2020-11-28 07:22:33 -0800

    add admin middleware; convert middleware from Router() to function

--- server/lib/routes/admin.route.ts
+++ server/lib/routes/admin.route.ts
@@ -1,36 +1,15 @@
-import * as express from "express";
-import * as admin from "firebase-admin";
+import { Request, Response, NextFunction } from "express";
 import User from "../models/users.model";
 
-const adminRoute = express.Router();
-adminRoute.use((req, res, next) => {
-  const { token } = req.headers;
-  admin
-    .auth()
-    .verifyIdToken(token as string)
-    .then(decodedToken => {
-      // check that the user exists in firebase
-      admin
-        .auth()
-        .getUser(decodedToken.uid)
-        .then(user => {
-          // check that the user is an admin of the site
-          User.find({uid: user.uid}, (error, data) => {
-            if (data.length !== 0 && data[0].admin !== true) {
-              res.status(401).send("The user with that ID does not have the proper credentials.");
-            } else {
-              res.locals.userid = user.uid;
-              next();
-            }
-          })
-        })
-        .catch(() => {
-          res.status(400).send("[oauth] The user with that ID does not exist.");
-        });
-    })
-    .catch((error) => {
-      res.status(401).send("Authentication failed.");
-    });
-});
+let adminRoute = (req: Request, res: Response, next: NextFunction) => {
+  // check if the user is an admin of the site
+  User.find({uid: res.locals.userid}, (error, data) => {
+    if (data[0].admin !== true) {
+      res.status(401).send("The user with that ID does not have the proper credentials.");
+    } else {
+      next();
+    }
+  });
+};
 
 export default adminRoute;
--- server/lib/routes/user.route.ts
+++ server/lib/routes/user.route.ts
@@ -1,11 +1,8 @@
-import * as express from "express";
 import * as admin from "firebase-admin";
-import { URL } from "url";
-const userRoute = express.Router();
-userRoute.use((req, res, next) => {
-  // User authentication middleware
+import { Request, Response, NextFunction } from "express";
+
+let userRoute = (req: Request, res: Response, next: NextFunction) => {
   const { token } = req.headers;
-  // const { pathname } = new URL(req.url);
   admin
     .auth()
     .verifyIdToken(token as string)
@@ -15,18 +12,21 @@ userRoute.use((req, res, next) => {
         .auth()
         .getUser(decodedToken.uid)
         .then((user) => {
-          res.locals.userid = user.uid;
-          next();
+          // check that the request and firebase IDs are the same
+          if (req.params.user_id !== undefined && req.params.user_id !== user.uid) {
+            res.status(401).send("The user in the header does not have access to the user passed in the request parameters.")
+          } else {
+            res.locals.userid = user.uid;
+            next();
+          }
         })
         .catch(() => {
-          res
-            .status(400)
-            .send("[oauth] The user with that ID does not exist.");
+          res.status(400).send("[oauth] The user with that ID does not exist.");
         });
     })
     .catch((error) => {
       res.status(401).send("Authentication failed.");
     });
-});
+};
 
 export default userRoute;
