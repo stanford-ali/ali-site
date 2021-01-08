984e97d5e02cba01dd95cf43db8a22027c9e5c96
Alix <acui@stanford.edu>
2021-01-08 18:40:16 -0500

    Update env variables

--- client/src/config/fbConfig.ts
+++ client/src/config/fbConfig.ts
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtJgb1-EaiakmzlNbVcDhHwzHMwwCkwkE",
-  authDomain: "applied-learning-initiat-dfcfe.firebaseapp.com",
-  databaseURL: "https://applied-learning-initiat-dfcfe.firebaseio.com",
-  projectId: "applied-learning-initiat-dfcfe",
-  storageBucket: "applied-learning-initiat-dfcfe.appspot.com",
-  messagingSenderId: "859859235960",
-  appId: "1:859859235960:web:c8dc4ee862b84ffd4ea98d",
-  measurementId: "G-VGGMVZL8Q1",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY || "AIzaSyDtJgb1-EaiakmzlNbVcDhHwzHMwwCkwkE",
+  authDomain: process.env.REACT_APP_AUTHDOMAIN || "applied-learning-initiat-dfcfe.firebaseapp.com",
+  databaseURL: process.env.REACT_APP_DATABASEURL || "https://applied-learning-initiat-dfcfe.firebaseio.com",
+  projectId: process.env.REACT_APP_PROJECTID || "applied-learning-initiat-dfcfe",
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET || "applied-learning-initiat-dfcfe.appspot.com",
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID || "859859235960",
+  appId: process.env.REACT_APP_APPID || "1:859859235960:web:c8dc4ee862b84ffd4ea98d",
+  measurementId: process.env.REACT_APP_MEASUREMENTID || "G-VGGMVZL8Q1",
 };
 
 export const uiConfig = {
